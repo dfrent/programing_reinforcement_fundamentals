3c3297b424d68f6ae0d80f5dfcbe3948dab3f91a
cordell <cordell.perry@gmail.com>
2017-11-08 14:33:40 -0500

    train scheduling part 6

--- programing_reinforcement_fundamentals.rb
+++ programing_reinforcement_fundamentals.rb
@@ -18,11 +18,28 @@ direction_six_ten = [direction: "north"]
 # puts eighty_b_frequency
 # puts direction_six_ten
 
-train_directions = []
-train_schedule.each do |key|
-  if key[:direction] == "north" || key[:direction] == "east"
-    train_directions << key
-  end
-end
+# train_directions = []
+# train_schedule.each do |key|
+#   if key[:direction] == "north" || key[:direction] == "east"
+#     train_directions << key
+#   end
+# end
+#
+# puts train_directions
 
-puts train_directions
+#create method accepting arguments of direction && trains
+#iterate over train_schedule
+# return list of trains for certain directions
+# call once for east and once for north
+
+
+def train_list(array, direction)
+route = []
+  array.each do |course|
+    if course[:direction] == direction
+      route << course[:train]
+    end
+    end
+    route
+end
+puts train_list(train_schedule, "east")
